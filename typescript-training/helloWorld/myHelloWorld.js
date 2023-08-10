8a3e72dce9de25c11bce28ac6e790382185a86a5
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-08-10 09:47:51 -0700

    intro to ts

--- typescript-training/helloWorld/myHelloWorld.js
+++ typescript-training/helloWorld/myHelloWorld.js
@@ -1,5 +1,4 @@
 console.log("Hello World!");
-
 //tsc compiles a javascript from the ts file
 // never edit the js file
-console.log("my name is crystal chavez")
+console.log("my name is crystal chavez");
